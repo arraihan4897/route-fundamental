2d9f09b653f1c0646b284e2242d47305087e489e
raihan <arraihan4897@gmail.com>
2023-05-17 05:04:01 +0600

    first commit

--- src/Componants/Country/Country.js
+++ src/Componants/Country/Country.js
@@ -8,7 +8,9 @@ const Country = () => {
             <img src={country.flags.png}  />
             <h1>name : {country.name.common}</h1>
             <h3>Capital: {country.capital}</h3>
-            <h3>Population: {country.population}</h3>           
+            <h3>Population: {country.population}</h3>
+
+            amar nam raihan bin solasoioman          
         </div>
     );
 };
